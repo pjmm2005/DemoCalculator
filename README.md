273acc538458799e6d07b16e8bfd54eae2424309
pjmm2005 <pjmm2005@gmail.com>
2022-01-12 16:17:16 +0100

    Added readme file and rest api documentation

---
+++ README.md
@@ -0,0 +1,59 @@
+# Calculator implementation
+
+
+## Purpose
+
+The objective of this application is to implement a microservice that represents a calculator and is therefore capable of performing a series of operations from two operands.
+
+This calculator in each implementation that we are going to use can be created with a series of operations, which can be added through existing operations or new operations.
+
+Later this calculator will be used through a REST api and therefore we will use SpringBoot to be able to make the corresponding requests and for the calculator to execute the corresponding operations.
+
+## Available operations
+
+As mentioned above, depending on the implementation of the calculator that is going to be used, it will have a series of operations or others, to be able to know at any time what operations we have, we can consult them in the url:
+
+```shell script
+calculator/availableOps
+```
+
+# Validating the application
+
+In order to validate the application, the first thing we will do is compile the application:
+
+```shell script
+mvn clean install
+```
+
+if we do not want the tests to be executed when the application is built
+
+```shell script
+mvn clean install -DskipTests
+```
+
+and later we can run the microservice locally with the command
+
+```shell script
+mvn spring-boot:run
+```
+
+# Testing the application
+
+The application contains both unit and integration tests, all of them can be executed with the command
+
+```shell script
+mvn clean verify
+```
+
+# API documentation
+
+We can consult both the available operations and the possible responses through open-api with swagger:
+
+```shell script
+http://localhost:8080/swagger-ui.html
+```
+
+# Logging the application
+
+The application has a tracing system through the 'tracer' library, leaving traces on the console.
+
--- src/main/java/com/calculator/demo/controllers/CalculatorController.java
+++ src/main/java/com/calculator/demo/controllers/CalculatorController.java
@@ -3,6 +3,11 @@ package com.calculator.demo.controllers;
 import com.calculator.demo.exceptions.OperationNotFoundException;
 import com.calculator.demo.exceptions.RestOperationNotFoundException;
 import com.calculator.demo.services.CalculatorService;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +15,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Calculator implementation demo API",
+        version = "1.0",
+        description = "API with the operations available for the calculator implementation"
+    )
+)
 @RestController
 @RequestMapping(path = "/calculator")
 public class CalculatorController {
@@ -17,6 +29,12 @@ public class CalculatorController {
   @Autowired
   CalculatorService calculatorService;
 
+  @Operation(summary = "Endpoint that allows the calculation through one operation and two operands.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successful response."),
+      @ApiResponse(responseCode = "400", description = "Not Found."),
+      @ApiResponse(responseCode = "500", description = "Internal server error.")
+  })
   @GetMapping(path = "/calculate", produces = "application/json")
   public ResponseEntity<Double> calculate(@RequestParam String operation, @RequestParam Double operand01, @RequestParam Double operand02){
     Double res = 0.0;
@@ -31,6 +49,11 @@ public class CalculatorController {
 
   }
 
+  @Operation(summary = "Endpoint that returns all available operations for a calculator implementation.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "Successful response."),
+      @ApiResponse(responseCode = "500", description = "Internal server error.")
+  })
   @GetMapping(path = "/availableOps", produces = "application/json")
   public ResponseEntity<String> getAvailableOps(){
 
